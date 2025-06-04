70bef13351c8b4b6ca711c5500013294cbeb9d71
chua <59224089+chua333@users.noreply.github.com>
2025-06-04 14:14:00 +0800

    playback audio + synthesizing audio output
    included the function for using audio wav track file for commands instead of microphones, also included the function for synthesizing the spoken output for a more different tone and pronunciations

--- ai-agent/speaking-clock.py
+++ ai-agent/speaking-clock.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from datetime import datetime
+from playsound import playsound
 import os
 
 # Import namespaces
@@ -20,8 +21,8 @@ def main():
         print("ready to use speech service in: ", speech_config.region)
 
         # Get spoken input
-        command = TranscribeCommand()
-        # command = "what time is it?"
+        # command = TranscribeCommand()
+        command = "what time is it?"
         if command.lower() == 'what time is it?':
             TellTime()
 
@@ -35,6 +36,13 @@ def TranscribeCommand():
     audio_config = speech_sdk.audio.AudioConfig(use_default_microphone=True)
     speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
     print("Listening for command...")
+
+    # # configure speech recognition using playback audio
+    # current_dir = os.getcwd()
+    # audioFile = current_dir + '\\time.wav'
+    # playsound(audioFile)
+    # audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    # speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
 
     # Process speech input
     speech = speech_recognizer.recognize_once_async().get()
@@ -58,10 +66,26 @@ def TellTime():
 
 
     # Configure speech synthesis
-    
+    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+
+    # # Synthesize spoken output
+    # speak = speech_synthesizer.speak_text_async(response_text).get()
+    # if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+    #     print(speak.reason)
 
     # Synthesize spoken output
-
+    responseSsml = " \
+        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
+            <voice name='en-GB-LibbyNeural'> \
+                {} \
+                <break strength='weak'/> \
+                Time to end this lab! \
+            </voice> \
+        </speak>".format(response_text)
+    speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
 
     # Print the response
     print(response_text)
